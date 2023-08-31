441052ce72ddb50cc97abefd396a0021243d3d6c
adithyan30 <adithyanramaligamb@gmail.com>
2023-08-31 15:00:18 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import Header from './components/Header';
+import './App.css'
+import { BrowserRouter, Router ,Route,Routes} from 'react-router-dom';
+import Home from './components/Home';
+import Cart from './Components/Cart';
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <BrowserRouter>
+    <Header></Header>
+    <div>
+    <Routes>
+      <Route path='/' exact Component={Home}></Route>        
+      <Route path="/cart" exact Component={Cart}></Route>
+      </Routes>
+    </div>
+    </BrowserRouter>
+  )
+}
+
+export default App
